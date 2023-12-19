cde90ce0d2316cb66885cfb2159d89ed3d6861a8
Mathilde Besse <mathildebesse07@gmail.com>
2023-12-19 15:46:41 +0100

    Ajout des flèches dans le fichier HTMl, Modification du fichier script avec ajout des bullets points, du carrousel d'image et du texte

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,56 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
-let arrow_left = document.querySelector(".arrow_left");
-arrow_left.addEventListener("click", ()=>{
-	console.log("Vous avez cliqué sur la flèche gauche")
+let i = 0;
+let arrowLeft = document.querySelector(".arrow_left");
+let arrowRight = document.querySelector(".arrow_right");
+let dots = document.querySelector(".dots");
+let Image = document.querySelector(".banner-img");
+let Texte = document.querySelector("p")
+
+//Fonction pour défilement des images à l'infini avec une condition
+function ChangementSlide(sens) {
+  i = i + sens;
+  if (i > 3) i = 0;
+  if (i < 0) i = 3;
+  Image.src = "./assets/images/slideshow/" + slides[i].image;
+  Texte.innerHTML = slides[i].tagLine
+}
+
+// Ajoutez des Event Listeners sur les flèches
+arrowLeft.addEventListener("click", () => {
+  ChangementSlide(-1);
+  console.log("Précédent");
 });
 
-let arrow_right = document.querySelector(".arrow_right");
-arrow_right.addEventListener("click", ()=>{
-	console.log("Vous avez cliqué sur la flèche droite")
+arrowRight.addEventListener("click", () => {
+  ChangementSlide(1);
+  console.log("Suivant");
 });
+
+// Ajoutez des bullet points au slider
+
+for (let D = 0; D < slides.length; D++) {
+  let dot = document.createElement("div");
+  dots.appendChild(dot);
+  dot.classList.add("dot");
+}
+let DotsEnfants = document.querySelectorAll(".dots .dot");
+DotsEnfants[0].classList.add("dot_selected");
